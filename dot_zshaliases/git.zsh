1379ebcc523a316ce826d8a43041494896c9d419
Dan Hughes <github@danhughes.dev>
2022-08-19 20:36:35 +0100

    Fix g alias on some computers but not others

--- dot_zshaliases/git.zsh
+++ dot_zshaliases/git.zsh
@@ -1,4 +1,9 @@
-g() {
+# This alias seems to be inconsistent, so unalias it if it exists.
+if which g >/dev/null 2>&1; then
+	unalias g
+fi
+
+function g() {
 	if [ "$#" -eq 0 ]; then
 		git status --short --branch
 	else
