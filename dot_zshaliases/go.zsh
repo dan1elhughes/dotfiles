ff835646f9ebfa262f06074eea6b6c7b0c0ab33f
Dan Hughes <github@danhughes.dev>
2023-07-03 17:24:22 +0100

    Watch lint

--- dot_zshaliases/go.zsh
+++ dot_zshaliases/go.zsh
@@ -25,3 +25,23 @@ watchbuild() {
 
     fd -e go | entr -c sh -c "go build ./... && echo ✅ || echo ❌"
 }
+
+# Watch all files for changes, and run linter when they change.
+watchlint () {
+        if ! command -v fd &> /dev/null
+        then
+                echo "fd not installed"
+                return 1
+        fi
+        if ! command -v entr &> /dev/null
+        then
+                echo "entr not installed"
+                return 1
+        fi
+        if ! command -v golangci-lint &> /dev/null
+        then
+                echo "golangci-lint not installed"
+                return 1
+        fi
+        fd -e go | entr -c sh -c "golangci-lint run && echo ✅ || echo ❌"
+}
