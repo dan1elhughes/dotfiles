f94db85ec05740dea541df64fe0f78faf7e0ae1b
Dan Hughes <github@danhughes.dev>
2024-08-12 18:31:39 +0100

    Add wezterm config

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,20 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+local appearance = require 'appearance'
+
+if appearance.is_dark() then
+  config.color_scheme = 'Ayu Mirage'
+else
+  config.color_scheme = 'Ayu Light (Gogh)'
+end
+
+config.font_size = 18
+config.font = wezterm.font({ family = 'Twilio Sans Mono' })
+config.line_height = 1.2
+
+config.window_decorations = 'RESIZE'
+config.window_frame = {
+  font = wezterm.font({ family = 'Twilio Sans Mono', weight = 'Bold' }),
+}
+
+return config
