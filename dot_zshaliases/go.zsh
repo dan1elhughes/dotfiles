3366ad03c301f47156a42ff4e5fa666062a7d075
Dan Hughes <github@danhughes.dev>
2025-11-03 12:10:47 +0000

    refactor go tests

--- dot_zshaliases/go.zsh
+++ dot_zshaliases/go.zsh
@@ -1,84 +1,72 @@
+# Helper function to check if a command is installed
+check-installed() {
+    local cmd=$1
+    if ! command -v "$cmd" &> /dev/null; then
+        echo "$cmd not installed"
+        return 1
+    fi
+}
+
+runtests() {
+    local envfile=$(git rev-parse --show-toplevel)/.env
+    local envfile_local=$(git rev-parse --show-toplevel)/.env.local
+
+    zsh -c "set -a; [ -f $envfile ] && source $envfile; [ -f $envfile_local ] && source $envfile_local; go test -p=1 ./... && echo ✅ || echo ❌"
+}
+
 # Watch all files for changes, and run tests when they change.
 watchtests() {
-    if ! command -v fd &> /dev/null; then
-        echo "fd not installed"
-        return 1
-    fi
-    if ! command -v entr &> /dev/null; then
-        echo "entr not installed"
-        return 1
-    fi
+    check-installed fd || return 1
+    check-installed entr || return 1
 
-    # Find the closest .env upwards
-	envfile=$(pwd)
-	while [ "$envfile" != "/" ]; do
-		if [ -f "$envfile/.env" ]; then
-			break
-		fi
-		envfile=$(dirname "$envfile")
-	done
+    local envfile=$(git rev-parse --show-toplevel)/.env
+    local envfile_local=$(git rev-parse --show-toplevel)/.env.local
 
-    fd -e go | entr -c sh -c "set -a; [ -f $envfile/.env ] && source $envfile/.env; go test -short -p=1 ./... && echo ✅ || echo ❌"
+    fd -e go | entr -c sh -c "set -a; [ -f $envfile ] && source $envfile; [ -f $envfile_local ] && source $envfile_local; go test -p=1 ./... && echo ✅ || echo ❌"
 }
 
 watchtest() {
-    if ! command -v fd &> /dev/null; then
-        echo "fd not installed"
-        return 1
-    fi
-    if ! command -v entr &> /dev/null; then
-        echo "entr not installed"
-        return 1
-    fi
-
-    # Find the closest .env upwards
-	envfile=$(pwd)
-	while [ "$envfile" != "/" ]; do
-		if [ -f "$envfile/.env" ]; then
-			break
-		fi
-		envfile=$(dirname "$envfile")
-	done
-
-    testName=$1
+    local testName=$1
     if [ -z "$testName" ]; then
         echo "Usage: watchtest <testName>"
         return 1
     fi
 
-    fd -e go | entr -c sh -c "set -a; [ -f $envfile/.env ] && source $envfile/.env; go test ./... -short -p=1 -run $testName && echo ✅ || echo ❌"
+    check-installed fd || return 1
+    check-installed entr || return 1
+
+    local envfile=$(git rev-parse --show-toplevel)/.env
+    local envfile_local=$(git rev-parse --show-toplevel)/.env.local
+
+    fd -e go | entr -c zsh -c "set -a; [ -f $envfile ] && source $envfile; [ -f $envfile_local ] && source $envfile_local; go test -p=1 ./... -run '$testName' && echo ✅ || echo ❌"
+}
+
+runtest() {
+    local testName=$1
+    if [ -z "$testName" ]; then
+        echo "Usage: runtest <testName>"
+        return 1
+    fi
+
+    local envfile=$(git rev-parse --show-toplevel)/.env
+    local envfile_local=$(git rev-parse --show-toplevel)/.env.local
+
+    zsh -c "set -a; [ -f $envfile ] && source $envfile; [ -f $envfile_local ] && source $envfile_local; go test -p=1 ./... -run '$testName' && echo ✅ || echo ❌"
 }
 
 # Watch all files for changes, and build when they change.
 watchbuild() {
-    if ! command -v fd &> /dev/null; then
-        echo "fd not installed"
-        return 1
-    fi
-    if ! command -v entr &> /dev/null; then
-        echo "entr not installed"
-        return 1
-    fi
+    check-installed fd || return 1
+    check-installed entr || return 1
 
     fd -e go | entr -c sh -c "go build ./... && echo ✅ || echo ❌"
 }
 
 # Watch all files for changes, and run linter when they change.
 watchlint () {
-        if ! command -v fd &> /dev/null
-        then
-                echo "fd not installed"
-                return 1
-        fi
-        if ! command -v entr &> /dev/null
-        then
-                echo "entr not installed"
-                return 1
-        fi
-        if ! command -v golangci-lint &> /dev/null
-        then
-                echo "golangci-lint not installed"
-                return 1
-        fi
-        fd -e go | entr -c sh -c "golangci-lint run && echo ✅ || echo ❌"
+    check-installed fd || return 1
+    check-installed entr || return 1
+    check-installed golangci-lint || return 1
+
+    fd -e go | entr -c sh -c "golangci-lint run && echo ✅ || echo ❌"
 }
